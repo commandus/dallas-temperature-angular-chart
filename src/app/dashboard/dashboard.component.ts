1ad4acbde973bc4404c9a53197cb03cd19d5a7b0
Andrei <andrei.i.ivanov@commandus.com>
2024-04-25 15:57:55 +0900

    1

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { SensorPlotComponent } from '../sensor-plot/sensor-plot.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
 
 // https://apexcharts.com/angular-chart-demos/line-charts/basic/
 // https://apexcharts.com/docs/angular-charts/
+
+class SensorMacNTemperature {
+  m = 0n;
+  t = 0.0;
+};
+
+class SensorItem {
+  m = 0n;
+  i = 0;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -14,12 +24,53 @@ import { NgApexchartsModule } from 'ng-apexcharts';
 })
 export class DashboardComponent implements OnInit{
   ev : EventSource = new EventSource("http://localhost:1234/event");
+  sensors: SensorItem[] = [];
+  @ViewChildren(SensorPlotComponent) plots! : QueryList<SensorPlotComponent>;
 
-  constructor() {
-    this.ev.onmessage = function(e) {
-      const d = e.data;
-      console.log(new Date(), d);
+  public put(mt: SensorMacNTemperature): boolean {
+    let r = false;
+    this.sensors.forEach(s => {
+      if (s.m == mt.m) {
+        r = true;
+      }
+    });
+    if (!r) {
+      const si = new SensorItem;
+      si.i = this.sensors.length;
+      si.m = mt.m;
+      this.sensors.push(si);
+      this.changeDetectorRef.detectChanges();
     }
+    this.plots.forEach(v => {
+      v.append(mt.t);
+      console.log(v);
+    });
+    return r;
+  }
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+    const that = this;
+    this.ev.onmessage = function(e) {
+      const mt = new SensorMacNTemperature;
+      const v = e.data.split(',');
+
+      for (var i = 0; i < v.length; i++) {
+        const pair = v[i].split('=');
+        if (pair.length == 2) {
+          if(pair[0] == 'm') {
+            mt.m = BigInt(pair[1]);
+          }
+          if(pair[0] == 't') {
+            console.log(pair[1]);
+            mt.t = Number.parseFloat(pair[1]);
+          }
+        }
+      }
+      that.put(mt);
+    }
+    
   }
 
   ngOnInit(): void {
--- src/app/sensor-plot/sensor-plot.component.ts
+++ src/app/sensor-plot/sensor-plot.component.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgApexchartsModule } from "ng-apexcharts";
+import { MatButtonModule } from "@angular/material/button";
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-sensor-plot',
   standalone: true,
-  imports: [],
+  imports: [NgApexchartsModule, MatButtonModule],
   templateUrl: './sensor-plot.component.html',
   styleUrl: './sensor-plot.component.scss'
 })
-export class SensorPlotComponent {
+export class SensorPlotComponent implements OnInit {
+  @ViewChild(ChartComponent) chart!: ChartComponent;
+  @Input() sensor = 0n;
 
+  public chartOptions: ChartOptions = {
+    series: [{
+        name: "Температура",
+        data: []
+      }
+    ],
+    chart: {
+      height: 350,
+      type: "line"
+    },
+    title: {
+      text: "Датчик"
+    },
+    xaxis: {
+      type: "datetime"
+    }
+  };
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Температура датчика " + (this.sensor ? this.sensor : " без номера"),
+          data: [
+          ]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line"
+      },
+      title: {
+        text: "Датчик " + (this.sensor ? this.sensor : " без номера")
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    };
+  }
+
+  apd() : void {
+    console.log(this.chart);
+    this.chart.appendSeries(
+      [{
+        name: "Температура датчика " + (this.sensor ? this.sensor : " без номера"),
+        data: []
+      }
+      ]
+    , true);
+  }
+
+  public append(t: number) : void {
+    console.log(this.chart);
+    this.chart.appendSeries([
+      {
+        name: '1',
+        data: [{ x: new Date().getTime(), y: t}]
+      }
+    ], true);
+  }
+ 
 }
